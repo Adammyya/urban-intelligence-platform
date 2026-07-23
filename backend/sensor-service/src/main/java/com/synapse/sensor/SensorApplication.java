8206555a1266c9e7841ea6a4882a8c31d02ab9dc
Adamya Pandey <adamya.16.pandey@gmail.com>
2026-07-23 23:30:32 +0530

    frontend

---
+++ backend/sensor-service/src/main/java/com/synapse/sensor/SensorApplication.java
@@ -0,0 +1,11 @@
+package com.synapse.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SensorApplication.class, args);
+    }
+}
